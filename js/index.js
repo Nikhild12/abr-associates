71dd323c0f0389e295c3f0e5b85635744d089fbb
Nikhild12 <ndhore8@gmail.com>
2020-08-04 22:05:21 +0530

    css changes are completed

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function panLink(){
+    window.open("https://www1.incometaxindiaefiling.gov.in/e-FilingGS/Services/LinkAadhaarHome.html?lang=eng");
+}
+
+function whatsAppLink(){
+    window.open("https://api.whatsapp.com/send?phone=919284190143&text=Hey+Abhijeet,+can+we+be+friends")
+}
